b5e4a32d0e5fd1588a2235523bbecd80d09eb749
Chris Smalley <chrism.smalley@gmail.com>
2018-10-01 10:18:21 -0500

    Added CSS, Customer and Movie Views, this completes exercise 2.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Fantastic Mr. Fox" },
+                new Movie { Id = 2, Name = "Harry Potter" },
+                new Movie { Id = 3, Name = "Forrest Gump" }
+            };
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+             {
+                 new Customer { Name = "Customer 1" },
+                 new Customer { Name = "Customer 2" }
+             };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
