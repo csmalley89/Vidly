9939fda7a9cdeeec5337cd9ab89f02b817a6db46
Chris Smalley <chrism.smalley@gmail.com>
2018-10-03 12:55:24 -0500

    Added form validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -20,20 +20,31 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
-
             return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
